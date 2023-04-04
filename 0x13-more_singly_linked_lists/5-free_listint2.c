555553c6d1ddbbc92c8cecec37f01cadd41bcf7d
oladunniwaters <waters.oladunni@gmail.com>
2023-04-04 13:44:33 -0700

    c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,18 +5,16 @@
  * free_listint2 - frees a listint_t list
  * @head: contain the first list
  **/
-
 void free_listint2(listint_t **head)
 {
-	listint_t *my_var;
+	listint_t *count;
 
 	if (head == NULL)
 		return;
-
 	while (*head != NULL)
 	{
-		my_var = (*head)->next;
+		count = (*head)->next;
 		free(*head);
-		*head = my_var;
+		*head = count;
 	}
 }
