0cc36bac097b03d9b05f14bd5088ff7bea7f618d
oladunniwaters <waters.oladunni@gmail.com>
2023-04-04 13:48:20 -0700

    c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,19 +2,20 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - frees a listint_t list
- * @head: contain the first list
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
  **/
+
 void free_listint2(listint_t **head)
 {
-	listint_t *count;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		count = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
-		*head = count;
+		*head = temp;
 	}
 }
