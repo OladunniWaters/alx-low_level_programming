ac1b9e626a95187dc0ec57644ce9d2af0768b4eb
oladunniwaters <waters.oladunni@gmail.com>
2023-04-09 06:08:20 -0700

    updates the file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,8 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int x, total;
 	
-	if (index > (sizeof(unsigned long int) * 8 -
-				1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	x = 1 << index;
 	total = n & x;
