e2cb6fe1bb89a374417bd171547c47735ad2b7aa
oladunniwaters <waters.oladunni@gmail.com>
2023-04-09 06:27:06 -0700

    c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	x = -(1 << index);
+	x = ~(1 << index);
 	*n = *n & x;
 
 	return (1);
